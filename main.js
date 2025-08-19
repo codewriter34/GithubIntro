7591a50e389669e3b5fb704e7badf3c2fab0c3d2
Forwamba <elishawamba61@gmail.com>
2025-08-19 11:33:20 +0100

    fea:test work

---
+++ main.js
@@ -0,0 +1,26 @@
+        // let name = "John";
+        // let age = 30;
+        // var occupation = "Developer";
+        
+        // console.log("My name is " + name + " and I am " + age + " years old." + "And i am a " + occupation + ".");
+        // console.log(age);
+        // console.log(occupation);
+
+       //operators in js
+//        let a =9
+//        let b =9
+//        console.log(a+b);
+//        console.log(a+=b);
+//        console
+
+//        var amount =100
+
+//         if(amount<100){
+//                 console.log("amount is too small")
+//         }
+//         else
+//         {
+//                 console.log("too big")
+//         }
+  //arrays
+// alert("Welcome Home");
